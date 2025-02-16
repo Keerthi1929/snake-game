a6274e168eb5506b6534ffa48a640d5e14eb46b0
sai keerthi <saikeerthiboda10@gmail.com>
2025-02-16 22:45:31 +0530

    food.py

--- snake game/food.py
+++ snake game/food.py
@@ -7,7 +7,7 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=0.1, stretch_wid=0.1)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("white")
         self.speed("fastest")
         self.refresh()
