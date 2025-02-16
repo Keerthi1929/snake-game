d89522db466407c62bf6fd75e3a273be8eee8c31
sai keerthi <saikeerthiboda10@gmail.com>
2025-02-16 22:32:26 +0530

    main.py

--- snake game/main.py
+++ snake game/main.py
@@ -12,7 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-score = Scoreboard()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -31,19 +31,19 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        score.increase_score()
+        scoreboard.increase_score()
         
     #detect collosion with wall
     if snake.head.xcor() > 280 or snake.head.xcor() > -280 or snake.head.ycor() > 280 or snake.head.ycor() > -280:
         game_is_on = False
-        score.game_over()
+        scoreboard.game_over()
    
     #detect collision with wall
     #if head collides with any segments with in the tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
            game_is_on = False
-           score.game_over()       
+           scoreboard.game_over()       
 
     
 screen.exitonclick()
