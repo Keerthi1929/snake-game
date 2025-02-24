66f7ebc95be4ca7537f2b79f3a7f60424c15290a
sai keerthi <saikeerthiboda10@gmail.com>
2025-02-24 18:19:57 +0530

    snake.py

--- snake game/snake.py
+++ snake game/snake.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 STARTING_POSITIONS =[(0,0), (-20,0), (-40,0)]
 MOVE_DISTANCE = 20
+SEGMENT_SIZE = 1
 UP = 90
 DOWN = 270
 LEFT = 180
@@ -22,8 +23,15 @@ class Snake:
         new_segment.color("white")
         new_segment.penup()
         new_segment.goto(position)
-        new_segment.shapesize(stretch_wid=0.5, stretch_len=0.5)  # Adjust the size here
+        new_segment.shapesize(stretch_wid= SEGMENT_SIZE, stretch_len= SEGMENT_SIZE)  # Adjust the size here
         self.segments.append(new_segment)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
         
     def extend(self):
         self.add_segment(self.segments[-1].position())
